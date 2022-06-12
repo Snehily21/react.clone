9cf83f2dbea714d21f75516e55a5543f669e1b88
Snehily21 <106708156+Snehily21@users.noreply.github.com>
2022-06-12 10:13:35 +0100

    Add files via upload

---
+++ precache-manifest.0c6fc53033b6490d5a76b7b86128ea6d.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f5fc12ff0a5888aee5fd6a7ae08b01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6571d47f52f3f6e3abd0",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "5eb3124a558ff1316f9c",
+    "url": "/static/css/main.495991f5.chunk.css"
+  },
+  {
+    "revision": "6571d47f52f3f6e3abd0",
+    "url": "/static/js/2.a071b0ad.chunk.js"
+  },
+  {
+    "revision": "094f9acebb3dfcde47a9c5956d931c9c",
+    "url": "/static/js/2.a071b0ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb3124a558ff1316f9c",
+    "url": "/static/js/main.6c048320.chunk.js"
+  },
+  {
+    "revision": "3083e94376b0a68fba84",
+    "url": "/static/js/runtime-main.ef3787f4.js"
+  },
+  {
+    "revision": "bb7ad0cf87dc877b8a19d5eb419a2aeb",
+    "url": "/static/media/about.bb7ad0cf.png"
+  },
+  {
+    "revision": "cb6b9b32bc84a5f59f3cfcf438cdc639",
+    "url": "/static/media/cart.cb6b9b32.svg"
+  },
+  {
+    "revision": "e975028cc6537bb1183fd4355a718834",
+    "url": "/static/media/community.e975028c.svg"
+  },
+  {
+    "revision": "33082d902a5167d3d725bb1f1fbae3c1",
+    "url": "/static/media/copy.33082d90.svg"
+  },
+  {
+    "revision": "ffffcd5b0446d65685581f1996544444",
+    "url": "/static/media/download.ffffcd5b.svg"
+  },
+  {
+    "revision": "a846854f0d2b49b7d54e8e4b6da2a20d",
+    "url": "/static/media/facebook.a846854f.svg"
+  },
+  {
+    "revision": "b9d30108f6d9fa6ea9f782e45aa42bb0",
+    "url": "/static/media/file.b9d30108.svg"
+  },
+  {
+    "revision": "1d682009728e5418f1c5186b3031f086",
+    "url": "/static/media/google.1d682009.svg"
+  },
+  {
+    "revision": "692f713dd7fe750baf28d89d2f863dd4",
+    "url": "/static/media/howitworks.692f713d.png"
+  },
+  {
+    "revision": "f4bc66e3238edf650406178fd616bf15",
+    "url": "/static/media/interface.f4bc66e3.svg"
+  },
+  {
+    "revision": "b4f3c6bcc708d531db4410a0b04c180c",
+    "url": "/static/media/linkedin.b4f3c6bc.svg"
+  },
+  {
+    "revision": "795fec94daf1ee0b679b60b237f29e3c",
+    "url": "/static/media/location.795fec94.svg"
+  },
+  {
+    "revision": "78103c5bd15048766c87ee0de33b0f1f",
+    "url": "/static/media/lock.78103c5b.svg"
+  },
+  {
+    "revision": "721fe978460725b71de95415a84236ec",
+    "url": "/static/media/logo_text.721fe978.png"
+  },
+  {
+    "revision": "3e806847a7ff188de5f2d207be386b41",
+    "url": "/static/media/mail.3e806847.svg"
+  },
+  {
+    "revision": "4eaece9a2374f90197315ff75b29b962",
+    "url": "/static/media/mobile_form.4eaece9a.png"
+  },
+  {
+    "revision": "ccf74e40481fc342e2559219a66e6f2c",
+    "url": "/static/media/phone.ccf74e40.svg"
+  },
+  {
+    "revision": "c18a6647c31adc5b3e375202e6e5cd04",
+    "url": "/static/media/pinterest.c18a6647.svg"
+  },
+  {
+    "revision": "60260b6fa32983912bebee43357d7b96",
+    "url": "/static/media/right_arrow.60260b6f.svg"
+  },
+  {
+    "revision": "cf56f6dd079517766d2443331c6f2bcc",
+    "url": "/static/media/ring.cf56f6dd.svg"
+  },
+  {
+    "revision": "2e22cf475881f04d19679c2e7589a2d6",
+    "url": "/static/media/roadmap.2e22cf47.png"
+  },
+  {
+    "revision": "89094e81a7180cfa2d502a71d7cb8350",
+    "url": "/static/media/roadmap1.89094e81.svg"
+  },
+  {
+    "revision": "11410601619be1b3aea34ade426a5e18",
+    "url": "/static/media/roadmap2.11410601.svg"
+  },
+  {
+    "revision": "1f51083b8f84d1f17778f035f0470a6c",
+    "url": "/static/media/roadmap3.1f51083b.svg"
+  },
+  {
+    "revision": "bb7048554450430a08264450f0466d4d",
+    "url": "/static/media/roadmap4.bb704855.svg"
+  },
+  {
+    "revision": "ce5dc96fe02b8c1d85c2c9d6abfab253",
+    "url": "/static/media/roadmap5.ce5dc96f.svg"
+  },
+  {
+    "revision": "bc286c35a1204cc72263567bae77692f",
+    "url": "/static/media/roadmap6.bc286c35.svg"
+  },
+  {
+    "revision": "d4264cd20ae8b58146094a3906a881ef",
+    "url": "/static/media/roadmap_vertical.d4264cd2.png"
+  },
+  {
+    "revision": "112f75d8f29c9fb4f0bcbe4f1db6f2ad",
+    "url": "/static/media/stats_card.112f75d8.png"
+  },
+  {
+    "revision": "9ab09d21409392861522ff162e3eb0e3",
+    "url": "/static/media/twitter.9ab09d21.svg"
+  }
+]);
